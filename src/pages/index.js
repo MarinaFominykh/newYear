6dc765ce240a783a42fa1d73e322ce65f0e5c703
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-10-06 19:00:21 +0300

    подвал, слайдер

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,33 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+  centeredSlides: false,
+  speed: 800,
+  simulateTouch: true,
+  grabCursor: true,
+  breakpoints: {
+    705: {
+      slidesPerView: 2,
+    },
+
+    800: {
+      slidesPerView: 2.5,
+    },
+    960: {
+      slidesPerView: 3,
+    },
+  },
+});
