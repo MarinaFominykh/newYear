688cffd94d32db641715dff4e3c7b640dcbdb583
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-10-07 20:41:24 +0300

    секция с баннером, секция расскажите о марафоне

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,25 @@
+const background = document.querySelector('.banner__svg')
+const banner = document.querySelector('.banner')
+
+const resizeListener = () => {
+const bannerWidth = window.getComputedStyle(banner).width
+const bannerHeight = window.getComputedStyle(banner).height
+const backgroundWidth = window.getComputedStyle(background).width
+const backgroundHeight = window.getComputedStyle(background).height
+background.style.width = bannerWidth
+background.style.height = bannerHeight
+  console.log("banner", {
+    width: bannerWidth,
+    height:bannerHeight
+  })
+    console.log("back", {
+    width: backgroundWidth,
+    height:backgroundHeight
+  })
+
+}
+// window.addEventListener("resize", resizeListener)
+
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
   spaceBetween: 20,
@@ -24,10 +46,14 @@ const swiper = new Swiper('.swiper', {
     },
 
     800: {
-      slidesPerView: 2.5,
+      slidesPerView: 2.3,
     },
     960: {
+      slidesPerView: 2.5,
+    },
+      1200: {
       slidesPerView: 3,
     },
+
   },
 });
